5f8290e2c87e1070687f7db32cb0d28e69828d4d
Sergey Lisovskiy <19lis91@gmail.com>
2024-12-09 21:01:27 +0300

    Exercise lambda

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (int i : intList) {
+            if (i > 0 && i % 2 == 0) {
+                result.add(i);
+            }
+        }
+        result.sort(Comparator.naturalOrder());
+        for (int i : result) {
+            System.out.println(i);
+        }
+
+
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        int count = (int) persons.stream().filter(person -> person.getAge() < 18).count();
+        System.out.println(count);
+
+        List<String> listFamilies = persons.stream()
+                .filter(person -> person.getAge() > 18 && person.getAge() < 27)
+                .map(person -> person.getFamily()).collect(Collectors.toList());
+
+        List<String> listWorking = persons.stream()
+                .filter(person -> person.getAge() > 18)
+                .filter(person -> (person.getSex().equals(Sex.MAN) && person.getAge() < 65)
+                        || (person.getSex().equals(Sex.WOMAN)) && person.getAge() < 60)
+                .map(person -> person.getFamily())
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+    }
+}
